3e214e568939971800bf3dca1bab46dee1d20ed5
umar710 <umarsyed082@gmail.com>
2024-05-17 13:20:59 +0530

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+
 app.use(express.json()); // Middleware to parse JSON bodies
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
   console.log(`Server is running on port 3000`);
 });
 
-mongoose.connect('mongodb+srv://umarsyed082:FN9hbncpXm65DJCe@cluster0.zcrk9j4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
